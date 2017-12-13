3ca87357eef60ab5f3e12259b08bf4f54027830c
vineethkartha <vineeth.kartha@gmail.com>
2017-12-14 00:11:33 +0530

    Clean the make files and restructure the folders 	renamed:    ds_gapbuffer/GapBuffer.cpp -> DataStructures/gapbuffer/GapBuffer.cpp 	new file:   DataStructures/gapbuffer/Makefile 	renamed:    ds_gapbuffer/include/GapBuffer.hpp -> DataStructures/gapbuffer/include/GapBuffer.hpp 	renamed:    ds_gapbuffer/tests/unittest.cpp -> DataStructures/gapbuffer/tests/unittest.cpp 	new file:   GUI/vt100/Makefile 	renamed:    gui_vt100/guilib.cpp -> GUI/vt100/guilib.cpp 	renamed:    gui_vt100/include/guilib.hpp -> GUI/vt100/include/guilib.hpp 	modified:   Makefile 	deleted:    ds_gapbuffer/Scribble 	deleted:    gui_vt100/#guilib.cpp# 	modified:   testMain.cpp

---
+++ src/DataStructures/gapbuffer/include/GapBuffer.hpp
@@ -0,0 +1,94 @@
+#include <string>
+/*
+Initialise the gap buffer with a given size
+initialise gap buffer with a given file
+
+* AddChar()
+* deleteChar()
+* resizeBuffer()
+* moveBuffer()
+
+Start with the buffer size and gap size as same
+Then keep adding the characters to the buffer
+If the cursor is moved then the gapStart moves to the current point
+
+
+Starting
+[__________] -> Gapsize = buffer Size =10
+Insert ABCD
+ABCD[______] -> Gapstart = insertCounter = 4
+Move Cursor before C
+AB[______]CD ->GapStart = CurrentPointer
+GapEnd = gapStart+ (gapSize-insertCounter)
+ */
+/**
+ * @brief This class represents the GapBuffer data structure
+ * 
+ * The Gap buffer https://en.wikipedia.org/wiki/Gap_buffer
+ * is the base data structure used by scribble
+ * This class implements the data structure.
+ */
+class GapBuffer {
+  
+  unsigned int gapSize;
+  unsigned int insertCounter = 0;
+  char *bufferStart;
+  char *bufferEnd;
+  char *gapStart;
+  char *gapEnd;
+  char *cursorPos;
+  char *buffer;
+public:
+  /**
+   *@brief Constructor
+   */
+  explicit GapBuffer(int gapSize);
+  //GapBuffer(FILE *fileH);
+  //GapBuffer(FILE *file, int gapSize);
+  ~GapBuffer();
+
+  /**
+   * @name Insert
+   * @brief This functions inserts the given character
+   * This function checks if the buffer is full. If it is full
+   * It resizes the buffer and inserts the character.
+   * @param symbol
+   */
+  void Insert(char symbol);
+
+  /**
+   * @name Delete
+   * @brief This function deletes the character at the current position.
+   */
+  void Delete();
+
+  /**
+   * @name Backspace
+   * @brief This function deletes the character previous to the current position.
+   */
+  void Backspace();
+  
+  /**
+   * @name MoveGap
+   * @brief This function moves the gap to wherever position mentioned.
+   * Based on the paramter pos, the gap is moved either forward
+   * or backward in the buffer. 
+   * @param int pos
+   */
+  void MoveGap(int pos);
+  
+  /**
+   * @name ResizeBuffer
+   * @brief When the buffer runs ou of space this function increases the buffer size.
+   */
+  void ResizeBuffer();
+  
+  /**
+   * @name printBuffer
+   * @brief This function returns the contents of the buffer.
+   * @return std::string
+   */
+  std::string printBuffer();
+  
+  void Debugprint();
+};
